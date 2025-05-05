e8a134e72723260423c589a78827e9d0fa74a5ec
rasyadsr <mchmdrasyad@gmail.com>
2025-05-05 14:44:55 +0700

    fetch api from api gateway

--- apps/frontend/employee/src/views/HomeView.vue
+++ apps/frontend/employee/src/views/HomeView.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useCounter } from '@frontend-lib/composable';
 import { useAuth } from '@frontend-lib/stores';
+import { onMounted } from 'vue';
+import { Employee } from '../types/employee';
 
 const { counter, increment, decrement } = useCounter();
 
 const store = useAuth();
+
+const employees = ref<Employee[]>([]);
+
+async function fetchEmployee() {
+  const response = await fetch('http://localhost:3000/api/employee');
+  const data = await response.json();
+  employees.value = data;
+}
+
+onMounted(() => {
+  fetchEmployee();
+});
 </script>
 
 <template>
   <main>
     <!-- <PayrollList /> -->
     <!-- <PayrollButton label="Di override ah" /> -->
+    <div>
+      <h1>Employee List</h1>
+      <ul>
+        <li v-for="employee in employees" :key="employee.id">
+          {{ employee.name }} - {{ employee.position }}
+        </li>
+      </ul>
+    </div>
     <div>
       <p>{{ JSON.stringify(store.user) }}</p>
       <button @click="store.login">Login</button>
